4619ddd68db0bef8341aca8cc2036a902713b6b4
Farazaulia <raz.muhammad14@gmail.com>
2019-11-11 18:24:07 +0700

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Faraz Muhammad Aulia on 11/11/19.
+//  Copyright © 2019 Faraz Muhammad Aulia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
